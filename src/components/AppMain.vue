0c23681529c52fd0623f7360df048033c9b42f78
marinodilauro <m.dilauro90@gmail.com>
2024-03-22 12:29:03 +0100

    Add dynamic products to the page
    - Put product list in a separated file
    - Put product card in a component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,70 +1,16 @@
 <script>
 
+import ProductCard from './ProductCard.vue';
+import { products } from '../data.js';
+
 export default {
   name: 'AppMain',
+  components: {
+    ProductCard
+  },
   data() {
     return {
-      productsList: [
-        {
-          image_1: '/images/1.webp',
-          image_2: '/images/1b.webp',
-          brand: "Levi's",
-          name: 'RELAXED FIT TEE UNISEX',
-          price: '29,99 €',
-          badge_1: '-50%',
-          badge_2: 'sustainability',
-          discounted_price: '14,99 €',
-          liked: false
-        },
-        {
-          image_1: '/images/2.webp',
-          image_2: '/images/2b.webp',
-          brand: 'Guess',
-          name: 'ROSES TEE',
-          price: '29,99 €',
-          badge_1: '-30%',
-          discounted_price: '20,99 €',
-          liked: false
-        },
-        {
-          image_1: '/images/3.webp',
-          image_2: '/images/3b.webp',
-          brand: 'Come Zucchero Filato',
-          name: 'VOGLIA DI COLORI PASTELLO',
-          price: '184,99 €',
-          badge_1: '-30%',
-          discounted_price: '129,99 €',
-          liked: false
-        },
-        {
-          image_1: '/images/4.webp',
-          image_2: '/images/4b.webp',
-          brand: "Levi's",
-          name: 'TEE UNISEX',
-          price: '29,99 €',
-          badge_1: '-50%',
-          badge_2: 'sustainability',
-          discounted_price: '14,99 €',
-          liked: false
-        },
-        {
-          image_1: '/images/5.webp',
-          image_2: '/images/5b.webp',
-          brand: 'Maya Deluxe',
-          name: 'STRIPE BODICE',
-          price: '99,99 €',
-          liked: false
-        },
-        {
-          image_1: '/images/6.webp',
-          image_2: '/images/6b.webp',
-          brand: 'Esprit',
-          name: 'MAGLIONE - BLACK',
-          price: '24,99 €',
-          badge_2: 'sustainability',
-          liked: false
-        },
-      ]
+      products
     }
   }
 }
@@ -73,51 +19,16 @@ export default {
 <template>
 
   <main id="site_main">
+
     <!-- Products list -->
-    <div class="products_container">
+    <section class="products_container">
 
-      <!-- Product 1 -->
-      <div v-for="(product) in productsList" class="product_card">
+      <ProductCard :product="product" :key="product.id" v-for="product in products" />
 
-        <!-- Product image -->
-        <div class="product_image">
+    </section>
 
-          <!-- Image -->
-          <div class="image">
-            <img :src="product.image_1" alt="">
-            <img class="hover_image" :src="product.image_2" alt="">
-          </div>
-          <!-- Like -->
-          <div class="like" @click="product.liked = !product.liked">
-            <i class="fa-solid fa-heart" :class="{ liked: product.liked === true }"></i>
-          </div>
-          <!-- Badges -->
-          <div class="badges">
-            <!-- Discount badge -->
-            <div v-if="product.badge_1" class="discount">{{ product.badge_1 }}</div>
-            <!-- Sustainability badge -->
-            <div v-if="product.badge_2" class="sustainability">{{ product.badge_2 }}</div>
-          </div>
-        </div>
-
-        <!-- Product description -->
-        <ul class="product_description">
-          <!-- Product brand -->
-          <li class="product_brand">{{ product.brand }}</li>
-          <!-- Product name -->
-          <li class="product_name">{{ product.name }}</li>
-          <!-- Product price -->
-          <li class="product_price">
-            <span v-if="product.discounted_price" class="discount_price">{{ product.discounted_price }}</span>
-            <span :class="product.discounted_price ? 'old_price' : 'normal_price'">{{ product.price }}</span>
-          </li>
-        </ul>
-      </div>
-
-    </div>
   </main>
 
 </template>
 
-<style lang="sass">
-</style>
+<style></style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  name: 'ProductCard',
+  props: {
+    product: Object
+  }
+}
+</script>
+
+<template>
+
+  <!-- Product -->
+  <div class="product_card">
+
+    <!-- Product image -->
+    <div class="product_image">
+
+      <!-- Image -->
+      <div class="image">
+        <img :src="'/images/' + product.frontImage" alt="">
+        <img class="hover_image" :src="'/images/' + product.backImage" alt="">
+      </div>
+
+      <!-- Like -->
+      <div class="like" @click="product.isInFavorites = !product.isInFavorites">
+        <i class="fa-solid fa-heart" :class="{ liked: product.isInFavorites === true }"></i>
+      </div>
+
+      <!-- Badges -->
+      <div class="badges">
+        <div v-for="badge in product.badges" :class="badge.type">{{ badge.value }}</div>
+      </div>
+
+    </div>
+
+    <!-- Product description -->
+    <ul class="product_description">
+
+      <!-- Product brand -->
+      <li class="product_brand">{{ product.brand }}</li>
+
+      <!-- Product name -->
+      <li class="product_name">{{ product.name }}</li>
+
+      <!-- Product price -->
+      <li class="product_price">
+        <span v-if="product.badges.find(e => e.type === 'discount')" class="discount_price">{{ (product.price -
+          (product.price * (product.badges.find(e => e.type === 'discount').value.substring(1, 3) / 100))).toFixed(2)
+          }} €</span>
+        <span :class="product.badges.find(e => e.type === 'discount') ? 'old_price' : 'normal_price'">{{ product.price
+          }} €</span>
+      </li>
+
+    </ul>
+
+  </div>
+
+</template>
+
+<style></style>
